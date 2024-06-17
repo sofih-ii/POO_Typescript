575532d09d2d3bad33ef1ad73e8e6e20d715d9fc
sofih-ii <sofiasaramoratrujillo@gmail.com>
2024-06-17 17:42:57 -0500

    Operation of class in TypeScript.

---
+++ src/01-class.ts
@@ -0,0 +1,24 @@
+const date = new Date();
+date.getHours();
+
+const date2 = new Date(2022, 4, 5);
+date2.getHours();
+date2.getTime();
+console.log(date2);
+
+
+class MyDate {
+    year: number;
+    month: number;
+    day: number;
+
+    constructor(year: number, month: number, day: number){
+        this.year = year
+        this.month = month;
+        this.day = day;
+    }
+}
+
+const myDate = new MyDate(2020, 3, 5);
+console.log(myDate);
+
